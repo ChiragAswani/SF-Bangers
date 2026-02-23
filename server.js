a66a94dfae721b0733434e2abdf5d5594aacf356
Chirag Aswani <chirag@aswani.net>
2026-02-23 15:14:30 -0800

    end to end implementation

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const admin = require('firebase-admin');
 const {getFirestore} = require('firebase-admin/firestore');
 const env = require('./vars/env.json');
 const credentials = require('./vars/credentials.json');
+const {getFoopeeConcertRangesByDate, getArtistsForFoopeeWeek} = require("./src/foopee");
+const {getSpotifyAccessTokenFromRefreshToken} = require("./src/generateSpotifyAccessToken");
+const {generatePlaylistTop5PerArtist} = require("./src/generateSpotifyPlaylist");
 
 const app = express();
 app.disable('etag');
@@ -16,6 +19,26 @@ app.use(express.urlencoded({ extended: false }));
 
 admin.initializeApp({credential: admin.credential.cert(credentials.GCP_SERVICE_ACCOUNT)});
 const db = getFirestore(app, 'sfbangers');
+
+app.get('/generate-playlist', async (req, res) => {
+    if (!req.query || !req.query.key || req.query.key !== 'ohBE0DPCNAlRv3lU') {
+        return res.status(401).send('Unauthorized');
+    }
+    const dateRanges = await getFoopeeConcertRangesByDate();
+    const selectedDateRange = dateRanges[0];
+    const artists = await getArtistsForFoopeeWeek(selectedDateRange);
+    const accessToken = await getSpotifyAccessTokenFromRefreshToken();
+    const playlistTitle = `SF Bangers / ${selectedDateRange}`;
+    const playlistDescription = `Auto generated playlist by SF Bangers for concerts in SF from ${selectedDateRange} powered by Foopee`
+    const playlistObj = await generatePlaylistTop5PerArtist(accessToken, artists, playlistTitle, playlistDescription);
+
+    const snap = await db.collection("playlists").where("isActive", "==", true).limit(1).get();
+    if (snap.empty) return;
+    await snap.docs[0].ref.update({ isActive: false });
+    await db.collection("playlists").add({ dateRange: selectedDateRange, playlistId: playlistObj.playlistId, isActive: true, });
+
+    return res.status(200).send(playlistObj.playlistId)
+});
 
 app.get('/get-playlists', async (req, res) => {
     const snapshot = await db.collection("playlists").get();
